ae3900f1bf71937bcbca46f622244e88a1597a0c
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-06-23 08:19:42 -0400

    Treino

--- Javalandia/src/javalandia/Projeto_Tennis/telas/Tela_Principal.java
+++ Javalandia/src/javalandia/Projeto_Tennis/telas/Tela_Principal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javalandia.Projeto_Tennis;
+package javalandia.Projeto_Tennis.telas;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
