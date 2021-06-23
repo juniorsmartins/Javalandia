3bb38d60418bd38ac1a9509183362f6508f25077
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-06-23 08:10:07 -0400

    Treino

---
+++ Javalandia/src/javalandia/Aulas_Livros/ArrayDeAlunos.java
@@ -0,0 +1,76 @@
+/*
+ * Exercício: Array de Alunos
+ * 
+ * Faça uma classe que contenha apenas um método main( ). Nesse método crie uma array de 5 posições
+ * do tipo Pessoa. Crie um objeto do tipo Aluno para cada posição da array. Especifique um nome, um endereço,
+ * uma data de nascimento, uma nota e uma turma para cada aluno. Imprima os dados de todos os alunos
+ * utilizando um laço for.
+ */
+package javalandia.Aulas_Livros;
+
+import AulasLivros.Pessoa;
+import java.util.Scanner;
+
+/**
+ * @author Junior Martins
+ * @since 29.12.20
+ * @version 1.0
+ */
+public class ArrayDeAlunos {
+    
+    public static void main(String[] args)
+    {
+        Scanner teclado = new Scanner(System.in);
+        Pessoa alunoJose = new Pessoa();
+        Pessoa alunoMario = new Pessoa();
+        Pessoa alunoMaria = new Pessoa();
+        
+        System.out.print("Digite nome: ");
+        alunoJose.nome = teclado.nextLine();        
+        System.out.print("Digite nota de Estatística: ");
+        alunoJose.notaEstatistica = teclado.nextFloat();
+        System.out.print("Digite nota de Algoritmo: ");
+        alunoJose.notaAlgoritmo = teclado.nextFloat();
+        System.out.print("Digite nota de Programação: ");
+        alunoJose.notaProgramacao = teclado.nextFloat();
+        System.out.println("------------------------------------");
+
+        System.out.print("Digite nome: ");
+        alunoMario.nome = teclado.next();        
+        System.out.print("Digite nota de Estatística: ");
+        alunoMario.notaEstatistica = teclado.nextFloat();
+        System.out.print("Digite nota de Algoritmo: ");
+        alunoMario.notaAlgoritmo = teclado.nextFloat();
+        System.out.print("Digite nota de Programação: ");
+        alunoMario.notaProgramacao = teclado.nextFloat();
+        System.out.println("------------------------------------");
+
+        System.out.print("Digite nome: ");
+        alunoMaria.nome = teclado.next();        
+        System.out.print("Digite nota de Estatística: ");
+        alunoMaria.notaEstatistica = teclado.nextFloat();
+        System.out.print("Digite nota de Algoritmo: ");
+        alunoMaria.notaAlgoritmo = teclado.nextFloat();
+        System.out.print("Digite nota de Programação: ");
+        alunoMaria.notaProgramacao = teclado.nextFloat();        
+        System.out.println("------------------------------------");
+
+        System.out.println("APRESENTAÇÃO");
+        System.out.println("Nome: " + alunoJose.nome);
+        alunoJose.fezProvas();
+        alunoJose.calculaMedia();
+        alunoJose.aprovadoOuNao();
+        
+        System.out.println("APRESENTAÇÃO");
+        System.out.println("Nome: " + alunoMario.nome);
+        alunoMario.fezProvas();
+        alunoMario.calculaMedia();
+        alunoMario.aprovadoOuNao();
+        
+        System.out.println("APRESENTAÇÃO");
+        System.out.println("Nome: " + alunoMaria.nome);
+        alunoMaria.fezProvas();
+        alunoMaria.calculaMedia();
+        alunoMaria.aprovadoOuNao();
+    }
+}
---
+++ Javalandia/src/javalandia/Projeto_Tennis/Tela_Principal.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javalandia.Projeto_Tennis;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+/**
+ * @author Junior Martins
+ */
+public class Tela_Principal 
+{
+// --------------- AREA DE ATRIBUTOS --------------- //
+    // ATRIBUTOS DE CLASSE
+    // ATRIBUTOS DE INSTANCIA
+
+// ---------------  AREA DE METODOS  --------------- //
+    // METODOS CONSTRUTORES
+    Tela_Principal()
+    {
+        initComponente();
+    }
+
+    // METODOS GETTERS E SETTERS
+    // METODOS FUNCIONAIS
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                new Tela_Principal();
+            }
+        });
+    }
+
+    private void initComponente()
+    {
+        // Cria conteiners superior
+        JFrame telaPrincipal = new JFrame("iTennis - Gestao Esportiva");
+        
+        // Dimensiona o tamanho do conteiner superior
+        telaPrincipal.setSize(1000, 600);
+        
+        // Determina o tipo de resposta ao fechamento da tela
+        telaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Cria componente rotulo
+        JLabel rot_login = new JLabel("LOGIN");
+        
+        // Adiciona componente rotulo ao painel de conteudo da tela principal
+        telaPrincipal.add(rot_login);
+        
+        // Torna visivel a tela principal
+        telaPrincipal.setVisible(true);
+    }
+
+    // METODOS ABSTRATOS E POLIMORFICOS
+}
+
+// --------------- AREA DE ATRIBUTOS --------------- //
+	// ATRIBUTOS DE CLASSE
+	// ATRIBUTOS DE INSTANCIA
+
+	// ---------------  AREA DE METODOS  --------------- //
+	// METODOS CONSTRUTORES
+	// METODOS GETTERS E SETTERS
+	// METODOS FUNCIONAIS
+	// METODOS ABSTRATOS E POLIMORFICOS
+
+	// ---------- Check List ----------
+	// Escrever codigo 
+	// Comentar codigo
+	// Documentar codigo
+	// Testar coidog
+	// Incluir mapeamento Objeto-relacional
