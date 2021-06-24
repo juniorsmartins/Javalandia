d5977fb66e093218b93225f23b5f7bd3576c9c76
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-06-24 19:06:13 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_certificacaoOCA/NomesMembrosClasse_1.java
@@ -0,0 +1,21 @@
+package javalandia.aulas_certificacaoOCA;
+
+public class NomesMembrosClasse_1 
+{
+    
+}
+
+class Test 
+{
+    int Test = 305;
+
+    void Test() 
+    {
+        System.out.println(Test);
+    }
+
+    public static void main(String[] args) 
+    {
+        new Test();
+    }
+}
