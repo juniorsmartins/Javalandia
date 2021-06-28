03fc4081d8b94e9f68e6416a31e8fc72b306a4b6
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-06-28 19:50:44 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_loianeGroner/AnimalDomesticado.java
@@ -0,0 +1,8 @@
+package javalandia.aulas_loianeGroner;
+
+public interface AnimalDomesticado 
+{
+    public void alimentar();
+    public void levarVeterinario();
+    public void chamarVeterinario();
+}
