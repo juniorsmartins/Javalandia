f71af79fb6113ea7a3db1efd7bef76c3d069465a
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-06-28 19:53:30 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_loianeGroner/Cachorro.java
@@ -0,0 +1,86 @@
+package javalandia.aulas_loianeGroner;
+
+import AulasLoianeGroner.AnimalEstimacao;
+import javalandia.aulas_loianeGroner.AnimalDomesticado;
+import javalandia.aulas_loianeGroner.Mamifero;
+
+public class Cachorro extends Mamifero implements AnimalDomesticado, AnimalEstimacao
+{
+    /* ---------- ÁREA DE ATRIBUTOS --------- */
+    // CONSTANTES
+    // Atributos de Classe
+    private static int totalCachorros;
+    
+    // Atributos de Instância
+    private String racaCachorro;
+    private String tamanho;
+
+    /* ---------- ÁREA DE MÉTODOS ---------- */
+    // Método Construtor
+    public Cachorro()
+    {
+        Cachorro.setTotalCachorros();
+    }
+    
+    // Métodos Especiais de Classe
+    private static void setTotalCachorros()
+    {
+        Cachorro.totalCachorros += 1;
+    }
+    public static int getTotalCachorros()
+    {
+        return Cachorro.totalCachorros;
+    }
+    
+    // Métodos Especiais de Instância
+    @Override
+    public void amamentar()
+    {
+        System.out.println("Cachorro amamenta");
+    }
+    @Override
+    public void emitirSom()
+    {
+        System.out.println("Cachorro emite som");
+    }
+    @Override
+    public void alimentar()
+    {
+        System.out.println("Cachorro alimentar");
+    }
+    @Override
+    public void levarVeterinario()
+    {
+        System.out.println("Cachorro levar veterinário");
+    }
+    @Override
+    public void chamarVeterinario()
+    {
+        System.out.println("Cachorro chamar veterinário");
+    }
+    @Override
+    public void brincar()
+    {
+        System.out.println("Cachorro brincar");
+    }
+    @Override
+    public void levarPassear()
+    {
+        System.out.println("Cachorro levar passear");
+    }
+    
+    // Métodos Getters e Setters
+    public String getRacaCachorro() {
+        return racaCachorro;
+    }
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setRacaCachorro(String raca) {
+        this.racaCachorro = raca;
+    }
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+}
