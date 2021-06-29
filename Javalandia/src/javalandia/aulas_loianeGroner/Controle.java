cc58c494a1fc91ad0485748d9987694db3c84f92
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-06-29 17:32:57 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_loianeGroner/Controle.java
@@ -0,0 +1,16 @@
+package javalandia.aulas_loianeGroner;
+
+import javalandia.aulas_loianeGroner.Atleta;
+
+public class Controle 
+{
+
+    public static void main(String[] args)
+    {
+        
+        Atleta atleta1 = new Atleta();
+        
+        
+        
+    }
+}
