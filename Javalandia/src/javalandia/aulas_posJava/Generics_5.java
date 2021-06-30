f1b9825b3805cdbfed52338ffae1dc8f812af7d8
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-06-30 17:01:36 -0400

    Treino

--- Javalandia/src/javalandia/aulas_posJava/Generics_5.java
+++ Javalandia/src/javalandia/aulas_posJava/Generics_5.java
@@ -2,5 +2,17 @@ package javalandia.aulas_posJava;
 
 public class Generics_5 
 {
+    private int sum;
     
+    <T extends Number> Generics_5(T arg)
+    {
+        sum = 0;
+        for(int i=0; i <= arg.intValue(); i++)
+        {
+            sum += i;
+        }
+    };
+    
+    public int getSum()
+    {return sum;};
 }
--- Javalandia/src/javalandia/aulas_posJava/Generics_Teste_5.java
+++ Javalandia/src/javalandia/aulas_posJava/Generics_Teste_5.java
@@ -1,6 +1,13 @@
 package javalandia.aulas_posJava;
 
+import static java.lang.System.out;
+
 public class Generics_Teste_5 
 {
-    
+    public static void main(String[] args)
+    {
+        Generics_5 num = new Generics_5(6.6);
+        
+        out.println("Resultado: " + num.getSum());
+    };
 }
