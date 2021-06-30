d307baa6ee810dbd6dd7494da813fafb3942bdd2
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-06-30 06:04:48 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_loianeGroner/CondicionalIf1.java
@@ -0,0 +1,19 @@
+package javalandia.aulas_loianeGroner;
+
+public class CondicionalIf1 {
+
+    public static void main(String[] args)
+    {
+        boolean temBatata = true;
+        int ovos = 6;
+        
+        if (temBatata) {
+            ovos = 9;
+        }
+        
+        System.out.println("Compre " + ovos + " ovos");
+    }
+
+
+    
+}
