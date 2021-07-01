c8e55badf5c093aae6248af609b377282cde419b
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-01 15:05:11 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_posJava/Generics_10.java
@@ -0,0 +1,40 @@
+package javalandia.aulas_posJava;
+
+import java.util.List;
+import java.util.ArrayList;
+import static java.lang.System.out;
+
+public class Generics_10 
+{
+    public static void main(String[] args) 
+    {
+        List<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        
+        int total = 0;
+        for(Integer i: lista)
+        {
+            total += i;
+        }
+        
+        out.println("Resultado: " + total);
+    };
+// sem uso do Generics
+//    public static void main(String[] args)
+//    {
+//        List lista = new ArrayList();
+//        lista.add(1);
+//        lista.add(2);
+//        lista.add(3);
+//        
+//        int total = 0;
+//        for(int i = 0; i < lista.size(); i++)
+//        {
+//            total += (int) lista.get(i);
+//        }
+//        
+//        out.println("Resultado: " + total);
+//    };
+};
