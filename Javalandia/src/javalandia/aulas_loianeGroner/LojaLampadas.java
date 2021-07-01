b706b1d4a3fe7b6170aaf6b773238b2f55bada57
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-01 07:51:44 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_loianeGroner/LojaLampadas.java
@@ -0,0 +1,25 @@
+
+package javalandia.aulas_loianeGroner;
+
+import AulasLoianeGroner.ProjetoLampada;
+
+public class LojaLampadas 
+{
+
+    public static void main(String[] args)
+    {
+        ProjetoLampada lamp01 = new ProjetoLampada();
+        ProjetoLampada lamp02 = new ProjetoLampada();
+
+        lamp02.ligarLampada();
+        lamp02.setMarcaLampada("Eletrical");
+        lamp02.setVoltagemLampada(4.5f);
+        lamp02.setPrecoLampada(9.99f);
+        lamp02.setTipoLampada("Fluorescente");
+        
+        
+        lamp01.apresentaLampada();
+        lamp02.apresentaLampada();
+    }
+    
+}
