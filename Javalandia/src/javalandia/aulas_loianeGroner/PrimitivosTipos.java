4a406d8d2c513e470a2ab7a0e0a0ce0591016952
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-02 19:45:33 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_loianeGroner/PrimitivosTipos.java
@@ -0,0 +1,33 @@
+package javalandia.aulas_loianeGroner;
+
+public class PrimitivosTipos {
+    
+    public static void main(String[] args)
+    {
+        // Tipos Primitivos (sem e com typecast)
+        // Inteiros
+        byte filhos1 = 1;
+        byte filhos2 = (byte) 12;
+        short dedos1 = 20;
+        short dedos2 = (short) 10;
+        int idade1 = 20;
+        int idade2 = (int) 22;
+        long maos1 = 2;
+        long maos2 = 2l;
+        long maos3 = (long) 2;
+        // Ponto Flutuante
+        float preco1 = 4.5f;
+        float preco2 = (float) 5.2;
+        double valor1 = 10.3;
+        double valor2 = (double) 9.6;
+        // Caracter
+        char letra1 = 'A';
+        char letra2 = (char) 'B';
+        // Boolean
+        boolean verdadeiro1 = true;
+        boolean verdadeiro2 = (boolean) true;
+        boolean falso1 = false;
+        boolean falso2 = (boolean) false;
+                
+    }
+}
