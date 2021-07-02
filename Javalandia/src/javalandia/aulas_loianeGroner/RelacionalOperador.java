ead28d2e63c4fc7568f8a4bac8fa0404c78d67e4
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-02 19:48:16 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_loianeGroner/RelacionalOperador.java
@@ -0,0 +1,29 @@
+package javalandia.aulas_loianeGroner;
+
+public class RelacionalOperador {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(" " + (1 > 0));
+        System.out.println(" " + (2 < 3));
+        System.out.println(" " + (4 != 4));
+        System.out.println(" " + (5 == 4));
+        System.out.println(" " + (5 == 5));
+        System.out.println(" " + (6 <= 7));
+        System.out.println(" " + (5 >= 10));
+        System.out.println(" " + (6 <= 6));
+        
+        int valor1 = 1;
+        int valor2 = 2;
+        
+        System.out.println("Valor1 = " + valor1 + " e valor2 = " + valor2);
+        System.out.println("Valor 1 == valor2: " + (valor1 == valor2));
+        System.out.println("Valor 1 != valor2: " + (valor1 != valor2));
+        System.out.println("Valor 1 > valor2: " + (valor1 > valor2));
+        System.out.println("Valor 1 >= valor2: " + (valor1 >= valor2));
+        System.out.println("Valor 1 < valor2: " + (valor1 < valor2));
+        System.out.println("Valor 1 <= valor2: " + (valor1 <= valor2));        
+    }
+    
+    
+}
