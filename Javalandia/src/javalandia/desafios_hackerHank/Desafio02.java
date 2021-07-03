2b79bc4d6b60429c3e6b1dad0e52acbc99000f0a
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-03 15:05:06 -0400

    Treino

---
+++ Javalandia/src/javalandia/desafios_hackerHank/Desafio02.java
@@ -0,0 +1,19 @@
+package javalandia.desafios_hackerHank;
+
+import java.util.Scanner;
+
+public class Desafio02 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
