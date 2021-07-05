aba79c1cd600b4fd0bc9cfb8c621b9baf761629b
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-05 09:51:47 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_soltas/MainCav.java
@@ -0,0 +1,21 @@
+package javalandia.aulas_soltas;
+
+import javalandia.aulas_soltas.ClassePessoas;
+import java.util.Scanner;
+
+public class MainCav {
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        ClassePessoas pessoa = new ClassePessoas();
+        
+        pessoa.status();
+
+    }
+    
+    
+    
+    
+}
