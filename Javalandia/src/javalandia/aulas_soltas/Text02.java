27c5bd5a38e0549ceb29c81caa271ac2b3259fdf
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-05 09:49:46 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_soltas/Text02.java
@@ -0,0 +1,19 @@
+package javalandia.aulas_soltas;
+
+// Teste de chaves
+
+public class Text02 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("1");
+        {
+            System.out.println("2");
+        }
+        {
+            System.out.println("3");
+        }
+        System.out.println("4");
+    }
+}
