a8d804e9352185f77506bcbf54d3ce1c2b5f7e70
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-06 05:36:18 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_soltas/Depurador.java
@@ -0,0 +1,18 @@
+package javalandia.aulas_soltas;
+
+public class Depurador 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Linha 1");
+        System.out.println("Linha 2");
+        System.out.println("Linha 3");
+        System.out.println("Linha 4");
+        System.out.println("Linha 5");
+        System.out.println("Linha 6");
+        System.out.println("Linha 7");
+        System.out.println("Linha 8");
+        System.out.println("Linha 9");
+        System.out.println("Linha 10");
+    }
+}
