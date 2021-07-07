91bf7ac852e5a98675d58c94c00afa5cb4751b44
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-07 18:43:26 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_soltas/TesteString.java
@@ -0,0 +1,19 @@
+package javalandia.aulas_soltas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteString 
+{
+    public static void main(String[] args)
+    {
+        ArrayList nums1 = new ArrayList();
+List nums2 = TesteString.returnList(nums1);
+        System.out.println(nums2);
+
+    }
+    public static List returnList(List list)
+    {
+        return list;
+    }
+}
