4435621462a4a3e365bd3a597d9748a600dc00ba
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-08 12:42:16 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_udemy/ProgramaPilha.java
@@ -0,0 +1,17 @@
+package javalandia.aulas_udemy;
+
+import AulasUdemy.AulaPilha;
+
+/*
+ * São estruturas de dados onde o elemento que está visível/disponível é o que está sempre no topo
+ * Novos elementos sempre são adicionados no topo das pilhas
+ * Só se pode remover elementos a partir do topo
+*/
+public class ProgramaPilha 
+{
+    public static void main(String[] args) 
+    {
+        AulaPilha pilha = new AulaPilha();
+        System.out.println(pilha);
+    }
+}
