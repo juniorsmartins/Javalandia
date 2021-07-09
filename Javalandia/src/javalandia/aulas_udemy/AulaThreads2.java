743d5b668fedc3eddf396deb2a2b64305574f945
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-09 10:32:57 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_udemy/AulaThreads2.java
@@ -0,0 +1,26 @@
+package javalandia.aulas_udemy;
+
+import AulasUdemy.Cliente;
+import AulasUdemy.Conta;
+import AulasUdemy.FazDeposito;
+
+public class AulaThreads2 
+{
+    public static void main(String[] args) throws InterruptedException 
+    {
+        Cliente cliente = new Cliente("Roberta Zanini");
+        Conta conta1 = new Conta(100);
+        
+        FazDeposito acao = new FazDeposito(conta1);
+        Thread th1 = new Thread(acao);
+        Thread th2 = new Thread(acao);
+        
+        th1.start();
+        th2.start();
+        
+        th1.join();
+        th2.join();
+        
+        System.out.println(conta1.getSaldoConta());
+    }
+}
