c17e89d8ddd301572ac0518dc7df3fd5a0b31990
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-09 10:36:15 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_udemy/AulaVetor.java
@@ -0,0 +1,25 @@
+package javalandia.aulas_udemy;
+
+public class AulaVetor 
+{
+    public static void main(String[] args)
+    {
+    // Declaração de vetor
+    int vetor[];
+    
+    // Declaração e definição de tamanho
+    short vet[] = new short[3];
+    
+    // Declaraçào de vetor do tipo byte e definição de tamanho
+    byte vetores[] = new byte[5];
+    
+    // Declaração, definição de tamanho e inicialização com valores
+    long vetorzinho[] = {1, 2, 3, 10, 55};
+    
+        System.out.println("0: " + vetorzinho[0]);
+        System.out.println("1: " + vetorzinho[1]);
+        System.out.println("1: " + vetorzinho[2]);
+        System.out.println("1: " + vetorzinho[3]);
+        System.out.println("1: " + vetorzinho[4]);
+    }
+}
