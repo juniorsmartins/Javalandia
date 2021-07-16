b251783ac88912d4528fb270e78a1d752f3fff2f
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-16 11:18:18 -0400

    Treino

--- Javalandia/src/javalandia/aulas_especializacaoJava/lambda/Aula_01.java
+++ Javalandia/src/javalandia/aulas_especializacaoJava/lambda/Aula_01.java
@@ -8,10 +8,23 @@ public class Aula_01
     public static void main(String[] args) 
     {
         double n = 2.0;
-        Aula_01_Interface varLambda; 
-        varLambda = () -> (98.4 + 1.6) * random();
+
+        // Declaração de variável lambda
+        Aula_01_Interface varLambda1;
+        Aula_01_Interface varLambda2;
+        Aula_01_Interface varLambda3;
+        Aula_01_Interface varLambda4;
+        Aula_01_Interface varLambda5;
+
+        // Uso de lambda em operações matemáticas
+        varLambda1 = () -> (98.4 + 1.6) * random();
+        varLambda2 = () -> 2.3 + 2.2;
         
-        out.println("Resultado: " + varLambda.metodoLambda() + "\n");
+        
+        
+        // Impressão do valor da variável lambda
+        out.println("Resultado: " + varLambda1.metodoLambda());
+        out.println(varLambda2.metodoLambda());
     }
 
 }
