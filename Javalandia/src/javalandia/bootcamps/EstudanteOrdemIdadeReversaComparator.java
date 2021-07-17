f745dcfccaa401c44fe9d50a19cc5355979cdb8a
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-17 18:31:52 -0400

    Treino

---
+++ Javalandia/src/javalandia/bootcamps/EstudanteOrdemIdadeReversaComparator.java
@@ -0,0 +1,13 @@
+package javalandia.bootcamps;
+
+import BootcampInterJavaDIO.AulaComparators.Estudante;
+import java.util.Comparator;
+
+public class EstudanteOrdemIdadeReversaComparator implements Comparator<Estudante>
+{
+    @Override
+    public int compare(Estudante o1, Estudante o2)
+    {
+        return o2.getIdade() - o1.getIdade();
+    }
+}
