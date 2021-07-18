d00f7f42d666bf06f008026fa6f11ddccb666e3d
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-18 12:19:20 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_especializacaoJava/IO_Read.java
@@ -0,0 +1,16 @@
+package javalandia.aulas_especializacaoJava;
+
+import java.io.IOException;
+
+public class IO_Read 
+{
+    public static void main(String[] args) throws IOException
+    {
+        byte data[] = new byte[15];
+        System.out.println("Enter some characters.");    
+        System.in.read(data);
+        System.out.print("You entered: "); 
+        for(int i=0; i < data.length; i++)
+            System.out.println((char) data[i]);
+    }
+}
