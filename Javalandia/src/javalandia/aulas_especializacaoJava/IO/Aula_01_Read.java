7cad0fc9147a19eeef0efd4f948e696140e547f7
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-18 15:06:23 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_especializacaoJava/IO/Aula_01_Read.java
@@ -0,0 +1,20 @@
+package javalandia.aulas_especializacaoJava.IO;
+
+import static java.lang.System.*;
+import java.io.IOException;
+
+public class Aula_01_Read 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        byte[] data = new byte[10];
+        
+        out.println("Olá, Mundo! Entre com caracteres: ");
+        int valor = System.in.read(data);
+        
+        for(int i = 0; i < valor; i++)
+        {
+            out.println((char) data[i]);
+        }
+    }
+}
---
+++ Javalandia/src/javalandia/aulas_especializacaoJava/IO/Aula_02_Write.java
@@ -0,0 +1,15 @@
+package javalandia.aulas_especializacaoJava.IO;
+
+public class Aula_02_Write 
+{
+    public static void main(String[] args) 
+    {
+        int b;
+        b = 'x';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+
+    
+    
+}
