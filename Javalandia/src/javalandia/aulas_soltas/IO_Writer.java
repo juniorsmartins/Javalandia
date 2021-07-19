c9863925df4391e3ea56a7f88dfe9014d87d050d
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-19 19:40:41 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_soltas/IO_Writer.java
@@ -0,0 +1,13 @@
+package javalandia.aulas_soltas;
+
+import java.io.*;
+
+public class IO_Writer 
+{
+    public static void main(String[] args) 
+    {
+        int b;
+        b = 'X';
+        System.out.write(b);
+    }
+}
