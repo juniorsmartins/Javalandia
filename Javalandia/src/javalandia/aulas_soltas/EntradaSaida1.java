148437e71c5731f77b4dcf723b519b32745f1c1e
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-19 19:38:14 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_soltas/EntradaSaida1.java
@@ -0,0 +1,27 @@
+package javalandia.aulas_soltas;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EntradaSaida1 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Entre com um valor: ");
+        InputStreamReader c = new InputStreamReader(System.in);
+        BufferedReader cd = new BufferedReader(c);
+        String s = "";
+
+        try
+        {
+                s = cd.readLine();
+        }
+        catch(IOException e)
+        {
+                System.out.println("Erro de entrada");
+        }
+
+        System.out.println("O valor de entradada foi: " + s);
+    }
+}
