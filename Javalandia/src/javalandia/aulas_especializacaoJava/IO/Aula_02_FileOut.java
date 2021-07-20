35b7c493c861b2402b2c450ccda852102b78e909
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-20 11:02:25 -0400

    Treino

--- Javalandia/src/javalandia/aulas_especializacaoJava/IO/Aula_02_FileOut.java
+++ Javalandia/src/javalandia/aulas_especializacaoJava/IO/Aula_02_FileOut.java
@@ -1,9 +1,55 @@
 package javalandia.aulas_especializacaoJava.IO;
 
+import static java.lang.System.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Aula_02_FileOut 
 {
     public static void main(String[] args) 
     {
+        String[] namesFiles = {"d:/javista.txt", "d:/chapeu.txt"};
         
+        int i;
+        FileInputStream fileIn = null;
+        FileOutputStream fileOut = null;
+        
+        if(namesFiles.length != 2)
+        {
+            err.format("Erro - Arquivos não encontrados!");
+            return;
+        }
+        
+        try
+        {
+            fileIn = new FileInputStream(namesFiles[0]);
+            fileOut = new FileOutputStream(namesFiles[1]);
+            
+            do
+            {
+                i = fileIn.read();
+                if(i != -1)
+                {
+                    fileOut.write(i);
+                }
+            }while(i != -1);
+        }
+        catch(IOException ioe)
+        {
+            err.format("Exceção: %s", ioe);
+        }
+        finally
+        {
+            try
+            {
+                if(fileIn != null)
+                {fileIn.close();}
+                if(fileOut != null)
+                {fileOut.close();}
+            }
+            catch(IOException io)
+            {err.format("Erro - Fechamento de arquivo!");}
+        }
     }
 }
---
+++ Javalandia/src/javalandia/aulas_especializacaoJava/IO/Aula_03.java
@@ -0,0 +1,9 @@
+package javalandia.aulas_especializacaoJava.IO;
+
+public class Aula_03 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
