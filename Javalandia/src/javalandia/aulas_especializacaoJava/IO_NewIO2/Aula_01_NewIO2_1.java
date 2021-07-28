8db32bf10150ef6374185426eccfcd144cd614e7
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-07-28 15:14:08 -0400

    Treino

---
+++ Javalandia/src/javalandia/aulas_especializacaoJava/IO_NewIO2/Aula_01_NewIO2_1.java
@@ -0,0 +1,29 @@
+package javalandia.aulas_especializacaoJava.IO_NewIO2;
+
+import java.io.File;
+
+public class Aula_01_NewIO2_1 
+{
+    private static final String MY_FILE = "d:" + File.separator + "javista.txt";
+
+    Aula_01_NewIO2_1()
+    {
+        writeFile();
+        readFile();
+    }
+
+    private void writeFile() 
+    {
+        
+    }
+
+    private void readFile() 
+    {
+        
+    }
+
+    public static void main(String[] args) 
+    {
+        new Aula_01_NewIO2_1();
+    }
+}
